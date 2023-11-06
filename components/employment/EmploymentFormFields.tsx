7c9720d01cd41b58c0eed598580c978a7a1cd010
Hafiz Temuri <temurih@live.co.uk>
2023-11-06 13:35:30 -0700

    fixing autoFocus issue

--- components/employment/EmploymentFormFields.tsx
+++ components/employment/EmploymentFormFields.tsx
@@ -16,7 +16,6 @@ const EmploymentFormFields: React.FC<FormFieldsProps> = (props) => (
             required
             type="text"
             id="name"
-            autoFocus
             placeholder="first name + last name + pronouns"
         />
         <Input required type="email" id="email" placeholder="email address" />
