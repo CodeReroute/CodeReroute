0aef346068a16a0bac77126234af1e7efdfcaa83
Hafiz Temuri <temurih@live.co.uk>
2024-12-09 20:47:36 -0700

    ...

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -7,7 +7,7 @@ import { white } from '../styles/theme';
 
 const StyledDiv = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 90svh;
     position: relative;
     padding-top: 20px;
     padding-bottom: 20px;
