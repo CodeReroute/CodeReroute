9179bc41b8a37c719955285ac9448f59e255023a
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2025-01-16 22:26:01 +0800

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#CDCCC8]">
-     <div className="flex flex-col text-black justify-center h-screen bg-[#CDCCC8]">
-      <h1 className="text-3xl font-bold font-goga">WE ARE UPDATING OUR SITE.</h1>
-      <h1 className="text-3xl text-start font-bold">CHECK BACK EARLY FEB.</h1>
+     <div className="text-black">
+      <h1 className="text-3xl font-goga">WE ARE UPDATING OUR SITE.</h1>
+      <h1 className="text-3xl font-goga">CHECK BACK EARLY FEB.</h1>
      </div>
 
     </div>
