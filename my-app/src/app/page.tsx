519f63309b35e8740aea224ee10bc87c79128402
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2025-01-16 22:07:43 +0800

    add temporary landing page

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-[#CDCCC8]">
+     <div className="flex flex-col text-black justify-center h-screen bg-[#CDCCC8]">
+      <h1 className="text-3xl font-bold font-goga">WE ARE UPDATING OUR SITE.</h1>
+      <h1 className="text-3xl text-start font-bold">CHECK BACK EARLY FEB.</h1>
+     </div>
+
+    </div>
+  )
+}
+
+export default page
