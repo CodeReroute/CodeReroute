9f2e57bc9bdd5182e442a9ac342994e28b0da335
Hafiz Temuri <temurih@live.co.uk>
2025-01-18 12:38:34 -0700

    add deployment workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#CDCCC8]">
-     <div className="text-black">
+     <div className="text-black p-10">
       <h1 className="text-3xl font-goga">WE ARE UPDATING OUR SITE.</h1>
       <h1 className="text-3xl font-goga">CHECK BACK EARLY FEB.</h1>
      </div>
