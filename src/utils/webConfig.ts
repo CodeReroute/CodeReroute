6a4e342cd7daec3093752c8e685d111329c68550
Hafiz Temuri <temurih@live.co.uk>
2025-08-25 23:46:02 -0600

    ...

--- src/utils/webConfig.ts
+++ src/utils/webConfig.ts
@@ -1,5 +1,5 @@
 export const webConfig = {
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   gaTrackingId: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
-  recaptchaV3Key: process.env.NEXT_PUBLIC_RECAPTCHAV3KEY,
+  recaptchaV3Key: process.env.NEXT_PUBLIC_RECAPTCHA_V3_KEY,
 };
