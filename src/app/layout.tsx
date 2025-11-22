5ac049da401b33d8df001950b735977d2d2d9780
Hafiz Temuri <temurih@live.co.uk>
2025-11-21 21:10:51 -0700

    update meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,28 +14,32 @@ const goga = localFont({
   variable: '--font-goga',
 });
 
-const title = 'Code Reroute';
-const description = 'Female-led tech company';
+const siteTitle = 'Code Reroute';
+const siteDescription = 'Female-led tech company';
 const keywords =
   'software development, canada, agency, edmonton, alberta, consultations, web design, software engineering, digital, app development, communications, web application, mobile application';
 const featuredImage = 'https://codereroute.com/sharing-meta-image.png';
 
 export const metadata: Metadata = {
-  title,
-  description,
-  keywords,
+  title: siteTitle,
+  description: siteDescription,
   icons: {
-    icon: '/favicon.ico',
+    icon: [
+      { url: '/favicon.ico', media: '(prefers-color-scheme: light)' },
+      { url: '/arrow-icon-light.png', media: '(prefers-color-scheme: dark)' },
+    ],
     shortcut: '/favicon.ico',
     apple: '/favicon.ico',
   },
+  keywords,
   openGraph: {
-    title,
-    description,
+    type: 'website',
     url: 'https://codereroute.com',
+    title: siteTitle,
+    description: siteDescription,
     images: [
       {
-        alt: title,
+        alt: siteTitle,
         url: featuredImage,
       },
     ],
@@ -46,6 +50,89 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <head>
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta
+          name="name"
+          content={siteTitle}
+        />
+        <meta
+          name="title"
+          content={siteTitle}
+        />
+        <meta
+          name="description"
+          content={siteDescription}
+        />
+        <meta
+          name="keywords"
+          content={keywords}
+        />
+        <meta
+          name="image"
+          content={featuredImage}
+        />
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta
+          property="og:url"
+          content="https://codereroute.com"
+        />
+        <meta
+          property="og:type"
+          content="website"
+        />
+        <meta
+          property="og:title"
+          content={siteTitle}
+        />
+        <meta
+          property="og:description"
+          content={siteDescription}
+        />
+        <meta
+          property="og:image"
+          content={featuredImage}
+        />
+        <meta
+          property="og:image:alt"
+          content={siteTitle}
+        />
+        <meta
+          property="fb:app_id"
+          content="811203999437355"
+        />
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
+        <meta
+          property="twitter:url"
+          content="https://codereroute.com"
+        />
+        <meta
+          property="twitter:title"
+          content={siteTitle}
+        />
+        <meta
+          property="twitter:image"
+          content={featuredImage}
+        />
+        <meta
+          property="twitter:description"
+          content={siteDescription}
+        />
+        <meta
+          name="twitter:title"
+          content={siteTitle}
+        />
+        <meta
+          name="twitter:description"
+          content={siteDescription}
+        />
+        <meta
+          name="twitter:image"
+          content={featuredImage}
+        />
         {webConfig.gaTrackingId && (
           <>
             <script
